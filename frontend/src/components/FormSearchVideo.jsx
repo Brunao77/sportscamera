317b2b62ab6cc67d5871289594a0b1fb10ed29f8
unknown <brucolon@gmail.com>
2024-04-19 11:46:44 -0300

    Refactoring code

--- frontend/src/components/FormSearchVideo.jsx
+++ frontend/src/components/FormSearchVideo.jsx
@@ -1,21 +1,22 @@
 import { useState, useEffect } from 'preact/hooks'
 import { getLastDays } from '../utils'
+import Select from './Select.jsx'
 
 export default function FormSearchVideo({options}) {
     const [selects, setSelects] = useState({establishment: {value: '', options:[]}, field: {value:'', options: []}, date: {value:'', options: []}, hour: {value:'', options: []}})
+    const [videosList, setVideoList] = useState(null)
 
     useEffect(()=>{
         const dates = getLastDays()
-        setSelects((prevState) => ({ ...prevState, establishment: {value: '',  options}, date: {value: '', options: dates} }))
+        setSelects((prevState) => ({ ...prevState, establishment: {value: '',  options}, date: {value: '', options: dates}}))
     }, [])
 
     const handleChangeEstablishment = async ({target}) =>{
-        const fields = await fetch(`http://localhost:3000/api/camera/${target.value}`).then((response) =>
-			response.json()
-		);
-		const optionsFields = fields.map(({camera_id, field_name})=> ({value:camera_id, text: field_name}))
-
-        setSelects((prevState) =>({ ...prevState, establishment: {value: target.value, options: prevState.establishment.options, isOpen: false}, field: {value: '', options: optionsFields}, date:{value:'', options:prevState.date.options}, hour: {value:'', options: []} }))
+        const optionsFields = await fetch(`/api/camera/${target.value}`).then(async (response) => {
+            const { cameras } = await response.json()
+            return cameras.map(({camera_id, field_name})=> ({value:camera_id, text: field_name}))
+        });
+        setSelects((prevState) =>({ ...prevState, establishment: {value: target.value, options: prevState.establishment.options}, field: {value: '', options: optionsFields}, date:{value:'', options:prevState.date.options}, hour: {value:'', options: []} }))
     }
 
     const handleChangeField = async ({target}) =>{
@@ -23,30 +24,37 @@ export default function FormSearchVideo({options}) {
     }
 
     const handleChangeDate = async ({target}) =>{
-        const selectedDate = target.value;
-        const selectedField = selects.field.value
-        
-
-        const turns = await fetch(`http://localhost:3000/api/videos/${selectedField}&${selectedDate}`).then((response) =>
+        const { videos } = await fetch(`/api/videos/videoTurns`,{
+            method:'POST',
+            body: JSON.stringify({camera_id: selects.field.value, date:target.value})
+        }).then((response) =>
 			response.json()
 		);
-
-        const turnsOptions = turns.map(({start_time, end_time})=>{
+        setVideoList(videos)
+        
+        const turnsOptions = videos.map(({start_time, end_time})=>{
             return {value: `${start_time}-${end_time}` , text: `${start_time.slice(0, -3)} - ${end_time.slice(0, -3)}`}
         })
         
-        setSelects((prevState) => ({ ...prevState, date: {value: selectedDate, options: prevState.date.options}, hour: {value: '', options: turnsOptions} }))
+        setSelects((prevState) => ({ ...prevState, date: {value: target.value, options: prevState.date.options}, hour: {value: '', options: turnsOptions} }))
+    }
+
+    const handleChangeHour = ({target}) =>{
+        setSelects((prevState) => ({ ...prevState, hour: {value: target.value, options: prevState.hour.options}}))
+    }
+
+    const handleSubmit = (event)=>{
+        event.preventDefault();
+
+        const selectedVideo = [...videosList].find(({start_time, end_time}) => {
+            return `${start_time}-${end_time}` ===  selects.hour.value
+        })
     }
   
     return (
         <>
-            <form class="flex flex-col gap-5 w-2/3">
-                <select required onChange={handleChangeEstablishment} class="border border-primary disabled:border-gray-200 border-2 w-full outline-none cursor-pointer rounded-lg p-2 focus:ring-primary focus:border-primary overflow-y-scroll">
-                    <option hidden >Predio</option>
-                    {selects.establishment.options && selects.establishment.options.map(({value, text})=>{
-                        return <option value={value}>{text}</option>
-                    })}
-                </select>
+            <form class="flex flex-col gap-5 w-2/3" onSubmit={handleSubmit}>
+                <Select placeholder='Predio' option={selects.establishment} handleOnChange={handleChangeEstablishment} />
                 <select required disabled={!selects.field.options.length} onChange={handleChangeField} value={selects.field.value} class="border w-full border-primary disabled:border-gray-200 border-2 outline-none cursor-pointer rounded-lg p-2 focus:ring-primary focus:border-primary">
                     <option hidden value=''>Cancha</option>
                     {selects.field.options && selects.field.options.map(({value, text})=>{
@@ -59,7 +67,7 @@ export default function FormSearchVideo({options}) {
                         return <option value={value}>{text}</option>
                     })}
                 </select>
-                <select required disabled={!selects.hour.options.length} value={selects.hour.value} class="border w-full border-primary disabled:border-gray-200 border-2 outline-none cursor-pointer rounded-lg p-2 focus:ring-primary focus:border-primary">
+                <select required disabled={!selects.hour.options.length} value={selects.hour.value} onChange={handleChangeHour} class="border w-full border-primary disabled:border-gray-200 border-2 outline-none cursor-pointer rounded-lg p-2 focus:ring-primary focus:border-primary">
                     <option hidden value=''>Hora</option>
                     {selects.hour.options.length ? selects.hour.options.map(({value, text})=>{
                         return <option value={value}>{text}</option>
--- frontend/src/components/Select.jsx
+++ frontend/src/components/Select.jsx
@@ -1,17 +1,12 @@
-export default function Select({placeholder, options, disabled, id}) {
-    console.log({options})
-  
+export default function Select({ placeholder, option, handleOnChange }) {
     return (
         <>
-            <div>
-                <slot />
-                <select disabled={disabled} id={id}>
-                    <option hidden>{placeholder}</option>
-                    {options && options.map(({value, text})=>{
-                        return <option value={value}>{text}</option>
-                    })}
-                </select>
-            </div>
+            <select required disabled={!option.options.length} onChange={handleOnChange} value={option.value} class="border border-primary disabled:border-gray-200 border-2 w-full outline-none cursor-pointer rounded-lg p-2 focus:ring-primary focus:border-primary overflow-y-scroll">
+                <option hidden value=''>{placeholder}</option>
+                {option.options && option.options.map(({value, text})=>{
+                    return <option value={value}>{text}</option>
+                })}
+            </select>
         </>
     )
-  }
+}

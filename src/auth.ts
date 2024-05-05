52a1a75c1633da7926abd187a1ab2f818cef4a3a
Brunao77 <brucolon@gmail.com>
2024-05-05 13:23:22 -0300

    Remove bcrypt

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,3 @@
-// src/auth.ts
 import { Lucia } from "lucia";
 import { NodePostgresAdapter } from "@lucia-auth/adapter-postgresql";
 import { pool } from "./models/db.js";
@@ -8,24 +7,16 @@ const adapter = new NodePostgresAdapter(pool, {
 	session: "user_session"
 });
 
-
 export const lucia = new Lucia(adapter, {
 	sessionCookie: {
 		attributes: {
-			// set to `true` when using HTTPS
 			secure: import.meta.env.PROD
 		}
 	},
-    getUserAttributes: (attributes) => {
+    getUserAttributes: ({ user_id, establishment_id }) => {
 		return {
-            user_id: attributes.user_id,
-            establishment_id: attributes.establishment_id
+            id: user_id,
+            establishment_id
 		};
 	}
 });
-
-declare module "lucia" {
-	interface Register {
-		Lucia: typeof lucia;
-	}
-}

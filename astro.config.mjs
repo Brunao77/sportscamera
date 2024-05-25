c06ac3c3c7dfa8afbcf3e395aeed622b6909622b
Brunao77 <brucolon@gmail.com>
2024-05-25 16:13:48 -0300

    build

--- astro.config.mjs
+++ astro.config.mjs
@@ -35,7 +35,7 @@ export default defineConfig({
         "stream",
         "tls",
         "path",
-        "argon2",
+        "hash",
       ],
     },
     plugins: [nodeResolve(), nodeExternals(), nodePolyfills()],

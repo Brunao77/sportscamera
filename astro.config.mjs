dfbe42660b491882cdbc7989a1dbd90742cdcce1
Brunao77 <brucolon@gmail.com>
2024-05-26 02:03:04 -0300

    build

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,7 @@ export default defineConfig({
   adapter: cloudflare(),
   vite: {
     ssr: {
-      external: ["oslo", "nodemailer", "pg"],
+      noExternal: ["oslo", "nodemailer", "pg"],
     },
   },
 });

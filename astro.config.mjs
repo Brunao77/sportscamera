5c75302496429e570bec1008a90d37dce08b5ed9
Brunao77 <brucolon@gmail.com>
2024-05-27 00:27:18 -0300

    client side localhost

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
-import cloudflare from "@astrojs/cloudflare";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import nodeExternals from "rollup-plugin-node-externals";
-import nodePolyfills from "rollup-plugin-node-polyfills";
 import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [
     preact({
